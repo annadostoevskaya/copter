86c0a947b8fe6da4d9c63b5850ecd499b1c14a95
Anna Dostoevskaya <iwantknow.aboutjt68h43@gmail.com>
2023-11-11 17:04:37 +0400

    feature: bluetooth

--- copter.ino
+++ copter.ino
@@ -1,18 +1,27 @@
 /**
- * File: copter.ino
- * Author: github.com/annadostoevskaya
- * Date: 11/10/2023 20:13:34
- * Last Modified Date: 11/10/2023 20:13:43
- */
+   File: copter.ino
+   Author: github.com/annadostoevskaya
+   Date: 11/10/2023 20:13:34
+   Last Modified Date: 11/10/2023 20:13:43
+   
+   41 - Vcc
+   43 - GND
+   45 - TX (for arduino RX)
+   47 - RX (for arduino TX)
+*/ 
 
 #include <Servo.h>
+#include <SoftwareSerial.h>
+
+SoftwareSerial bluetooth(45, 47);
 
 Servo motors[4];
 
-void setup() 
+void setup()
 {
   Serial.begin(115200);
-
+  bluetooth.begin(9600);
+  
   int shift = 2;
   int port = 0;
   for (int i = 0; i < 4; i += 1)
@@ -28,13 +37,23 @@ void setup()
 
   for (int i = 0; i < 4; i += 1)
     motors[i].writeMicroseconds(800);
-    
+
   delay(6000);
+
+  // Bluetooth Module Init
+  pinMode(41, OUTPUT);
+  digitalWrite(41, HIGH);
+  pinMode(43, OUTPUT);
+  digitalWrite(43, LOW);
 }
 
-void loop() 
+void loop()
 {
-  int stick = map(analogRead(0), 0, 1023, 800, 2300);
-  for (int i = 0; i < 4; i += 1)
-    motors[i].writeMicroseconds(stick);
+  ////////////////////////////////////////////////////////// 
+  // int stick = map(analogRead(0), 0, 1023, 800, 2300);
+  // for (int i = 0; i < 4; i += 1)
+  //   motors[i].writeMicroseconds(stick);
+
+  ////////////////////////////////////////////////////////// 
+  bluetooth.println("You look lonely, I can fix that...");
 }
